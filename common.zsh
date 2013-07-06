837995fa868f709715a4a6f0a2270ecde4eb52eb
Lucas A. Dohring <lucas@dohring.tk>
2013-07-06 16:11:18 -0700

    Licensed under EUPL

--- common.zsh
+++ common.zsh
@@ -1,4 +1,27 @@
 #!/bin/zsh
+: <<EOF
+Copyright 2013 Lucas A. Dohring
+
+Licensed under the EUPL, Version 1.1 or – as soon they
+will be approved by the European Commission - subsequent
+versions of the EUPL (the "Licence");
+
+You may not use this work except in compliance with the
+Licence.
+
+You may obtain a copy of the Licence at:
+http://ec.europa.eu/idabc/eupl
+
+Unless required by applicable law or agreed to in
+writing, software distributed under the Licence is
+distributed on an "AS IS" basis,
+
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.
+
+See the Licence for the specific language governing
+permissions and limitations under the Licence.
+EOF
 
 readonly NICKRE='^[[:alpha:]][[:alnum:]\-\[-\`_-}]{0,8}$'
 readonly CHANRE='[#&][^\0\a\r\n ,:]{1,200}'
